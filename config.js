7d2f2bfc4f917dddd366fe2bbdfe9f4aa2855d0e
yushi512 <y2831451@gmail.com>
2025-11-12 16:03:51 +0900

    delete gitignore

--- config.js
+++ config.js
@@ -3,12 +3,13 @@ window.MITASAI_CONFIG = {
    * Google Apps Script で公開した Web アプリの URL。
    * 例: https://script.google.com/macros/s/XXXXX/exec
    */
-  sheetsEndpoint: "",
+  sheetsEndpoint:
+    "https://script.google.com/macros/s/AKfycbwS6cF3JKJK35fcgajPXpjABEIvA1t9Fv6FfzdVC4naJLdMZyoiCmr35kNI_FkV_Uhc/exec",
 
   /**
    * 任意の共有用キー。Apps Script 側で照合して簡易認証に使います。
    */
-  sheetsApiKey: "",
+  sheetsApiKey: "keio-rover-mitasai-chasher",
 
   /**
    * フロントエンドからの送信タイムアウト (ミリ秒)
